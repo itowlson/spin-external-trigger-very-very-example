b286835db93041a83d5e4695c4f153ef2fd15a7b
itowlson <ivan.towlson@fermyon.com>
2023-01-19 17:20:03 +1300

    Rather silly test of passing app-level trigger settings

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,15 @@ async fn main() -> Result<(), Error> {
 
 struct TimerTrigger {
     engine: TriggerAppEngine<Self>,
-    component_timings: HashMap<String, tokio::time::Duration>,
+    speedup: u64,
+    component_timings: HashMap<String, u64>,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+struct TriggerMetadata {
+    r#type: String,
+    speedup: Option<String>,
 }
 
 #[async_trait::async_trait]
@@ -34,12 +42,19 @@ impl TriggerExecutor for TimerTrigger {
     type RunConfig = spin_trigger::cli::NoArgs;
 
     fn new(engine: spin_trigger::TriggerAppEngine<Self>) -> anyhow::Result<Self>  {
+        let speedup = engine
+            .app()
+            .require_metadata::<TriggerMetadata>("trigger")?
+            .speedup
+            .map(|s| s.parse::<u64>())
+            .unwrap_or(Ok(1))?;
+
         let component_timings = engine
             .trigger_configs()
-            .map(|(_, config)| (config.component.clone(), secs_text_to_dur(&config.interval_secs)))
+            .map(|(_, config)| (config.component.clone(), config.interval_secs.parse::<u64>().unwrap()))
             .collect();
 
-        Ok(Self { engine, component_timings })
+        Ok(Self { engine, speedup, component_timings })
     }
 
     async fn run(self, _config: Self::RunConfig) -> anyhow::Result<()> {
@@ -47,12 +62,15 @@ impl TriggerExecutor for TimerTrigger {
             tokio::signal::ctrl_c().await.unwrap();
             std::process::exit(0);
         });
+
+        let speedup = self.speedup;  // We could do all this in `new` I know - I am trying to get a sense for how data percolates around the trigger
         tokio_scoped::scope(|scope|
             for (c, d) in &self.component_timings {
                 scope.spawn(async {
+                    let duration = tokio::time::Duration::from_millis(*d * 1000 / speedup);
                     loop {
                         self.handle_timer_event(c).await.unwrap();
-                        tokio::time::sleep(*d).await;
+                        tokio::time::sleep(duration).await;
                     }
                 });
             }
@@ -61,10 +79,10 @@ impl TriggerExecutor for TimerTrigger {
     }
 }
 
-fn secs_text_to_dur(interval_secs: &str) -> tokio::time::Duration {
-    let seconds: u64 = interval_secs.parse().unwrap();
-    tokio::time::Duration::from_secs(seconds)
-}
+// fn secs_text_to_dur(interval_secs: &str) -> tokio::time::Duration {
+//     let seconds: u64 = interval_secs.parse().unwrap();
+//     tokio::time::Duration::from_secs(seconds)
+// }
 
 impl TimerTrigger {
     async fn handle_timer_event(&self, component_id: &str) -> anyhow::Result<()> {
